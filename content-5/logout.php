0d022896514c885312547b7ffbd7d654123016a6
rbm1718 <rami@muleys.ru>
2017-03-23 06:17:39 +0300

    [159] in [logout.php]

--- content-5/logout.php
+++ content-5/logout.php
@@ -3,6 +3,7 @@ session_start();
 session_unset();
 session_destroy();
 if(isset($_COOKIE['User'])) {
+  // TODO: AI issue #7, High, 159, https://github.com/rbm1718/bricks/issues/7
   unset($_COOKIE['User']);
   setcookie('User', '', time() - 3600);
 }
