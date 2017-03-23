b3d5567a37013b04f3bf24b4e774706beb45a506
rbm1718 <rami@muleys.ru>
2017-03-23 06:17:23 +0300

    [Sensitive Cookie in HTTPS Session Without ‘Secure’ Attribute] in [logout.php]

--- content-5/logout.php
+++ content-5/logout.php
@@ -4,6 +4,7 @@ session_unset();
 session_destroy();
 if(isset($_COOKIE['User'])) {
   unset($_COOKIE['User']);
+  // TODO: AI issue #5, High, Sensitive Cookie in HTTPS Session Without ‘Secure’ Attribute, https://github.com/rbm1718/bricks/issues/5
   setcookie('User', '', time() - 3600);
 }
 header("location:index.php");
